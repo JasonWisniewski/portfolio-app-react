19c858f5ec401c1bca12ea2e0d9389d191c87b18
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-20 13:52:22 -0600

    about me styling

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 import headShot from "../../assets/images/HeadShot.jpg";
 
 const About = () => {
   return (
-    <div className="p-3">
-      <h1>About Me</h1>
-      <img src={headShot}></img>
-      <p>
-        I am a Full Stack Web Developer experienced in creating mobile-first websites and applications.  Skilled in writing clean, efficient DRY code.  Highly motived self-starter, comfortable in team environment, eager to utilize my skills to contribute to your business and continue to develop my skills.  Checkout projects I have completed below in the projects section.
-      </p>  
+    <div>
+      <h1 className="m-2 p-1">About Me</h1>
+      <div className="p-3 row">
+        <div className="col-sm-3">
+          <img src={headShot}></img>
+        </div>
+        <div className="col-sm-9">
+          <p>
+            I am a Full Stack Web Developer experienced in creating mobile-first
+            websites and applications. Skilled in writing clean, efficient DRY
+            code. Highly motived self-starter, comfortable in team environment,
+            eager to utilize my skills to contribute to your business and
+            continue to develop my skills. Checkout projects I have completed
+            below in the projects section.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
