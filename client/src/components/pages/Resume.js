7f32cafc8f2ac7c94c92723b94a58addebe91165
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-20 14:11:02 -0600

    resume section completed with downloadable pdf

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import CV from "../../assets/images/Resume.png";
 
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
+    <div>
+      <div>
+        <div className="p-2 m-2 justify-content-center">
+          you can download a pdf of my resume
+          <a href="./directory/yourfile.pdf" download="newfilename">
+            Here!
+          </a>
+        </div>
+        <img className="p-2 m-2" src={CV}></img>
+      </div>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
