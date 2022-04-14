7109fb7815d7440bb3c454443207da4637d57669
Jason Wisniewski <jwizinpa95@gmail.com>
2022-04-14 12:41:00 -0600

    footer icons created

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="bg-gunmetal text-white pt-3 pb-3">
-      <div className="container text-center text-md-left">
-        <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-          <h5 className="mb-4 font-weight-bold text-warning">
-            
-            <ul>
-              <li>
-                <a href="https://github.com/JasonWisniewski">GitHub</a>
-              </li>
-              <li>
-                <a href="mailto:jwizinpa95@gmail.com">Email</a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/jason-r-wisniewski-572a1a155/">LinkedIn</a>
-              </li>
-              <li>
-                <a href="https://stackoverflow.com/users/17072525/jason-wisniewski">Stack Overflow</a>
-              </li>
-            </ul>
-          </h5>
-        </div>
-      </div>
-    </footer>
-  )
-}
+    <footer className="bg-gunmetal pt-3 pb-3 d-flex justify-content-around">
+      
+      <a href="https://github.com/JasonWisniewski">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
 
-export default Footer
+      <a href="mailto:jwizinpa95@gmail.com">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+
+      <a href="https://www.linkedin.com/in/jason-r-wisniewski-572a1a1">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+
+      <a href="https://stackoverflow.com/users/17072525/jason-wisniewski">
+        <FontAwesomeIcon icon={faStackOverflow} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
